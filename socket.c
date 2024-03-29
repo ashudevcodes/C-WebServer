46eb0f2a11f623cb7293aaf341feb65e39645b36
ashish <ashishprasad949@gmail.com>
2024-03-29 16:33:28 +0530

    linting

--- socket.c
+++ socket.c
@@ -7,12 +7,11 @@
 
 void main() {
   int s = socket(AF_INET, SOCK_STREAM, 0);
-  struct sockaddr_in addr =
-      {
-          AF_INET,
-          0x901f,
-          0,
-      };
+  struct sockaddr_in addr = {
+      AF_INET,
+      0x901f,
+      0,
+  };
 
   bind(s, &addr, sizeof(addr));
   listen(s, 10);
@@ -22,7 +21,7 @@ void main() {
   recv(client_fd, buffer, 256, 0);
   char *f = buffer + 5;
   *strchr(f, ' ') = 0;
- int opend_fd = open(f, O_RDONLY);
+  int opend_fd = open(f, O_RDONLY);
   sendfile(client_fd, opend_fd, 0, 256);
   close(opend_fd);
   close(client_fd);
